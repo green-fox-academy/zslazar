ed03867fcc3fd10e443c109b9d571f69bbdf6939
zslazar <zs.lazar@gmail.com>
2017-11-28 00:29:50 +0100

    exercise-15 done

---
+++ week-02/day-01/Exercise-15/Exercise-15/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Write a program that asks for an integer that is a distance in kilometers,
+            // then it converts that value to miles and prints it
+
+            Console.WriteLine("Hello user what is the distance in kilometers?");
+            string datakm = Console.ReadLine();
+            int datam = int.Parse(datakm);
+             
+            Console.WriteLine("In miles it is: " + datam * 0.621);
+
+            Console.ReadLine();
+
+        }
+    }
+}
