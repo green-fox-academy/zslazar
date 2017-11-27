d2f2457f19c6deda773b651767eeeab69faab99f
zslazar <zs.lazar@gmail.com>
2017-11-27 16:05:31 +0100

    exercise01-02-04

---
+++ week-02/day-01/Exercise-04/Exercise-04/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zsuzsa Lazar");
+
+            Console.WriteLine("21 ;)");
+
+            Console.Write(1.68);
+
+            Console.Read();
+        }
+    }
+}
