9244d8556d0c2d5f98f0ac312bf30322ef23d96b
zslazar <zs.lazar@gmail.com>
2017-11-27 23:33:32 +0100

    exercise-07 done

---
+++ week-02/day-01/Exercise-07/Exercise-07/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int value = 144;
+            Console.WriteLine("My favorite number is: " + value);
+
+            Console.ReadLine();
+        }
+    }
+}
