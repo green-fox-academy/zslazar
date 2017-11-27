723443f21daa517d0c1c44859842605bcd04a023
zslazar <zs.lazar@gmail.com>
2017-11-27 23:52:42 +0100

    exercise-10 done

---
+++ week-02/day-01/Exercise-10/Exercise-10/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Define several things as a variable then print their values
+            // Your name as a string
+
+            string name = "Zsuska";
+            Console.WriteLine("myname: " + name);
+
+            // Your age as an integer
+
+            int age = 21;
+            Console.WriteLine("myage: " + age);
+
+            // Your height in meters as a double
+
+            double height = 1.69;
+            Console.WriteLine("myheight: " + height);
+
+
+            // Whether you are married or not as a boolean
+
+            bool married = false;
+            Console.WriteLine("married state: " + married);
+
+            Console.ReadLine();
+        }
+    }
+}
