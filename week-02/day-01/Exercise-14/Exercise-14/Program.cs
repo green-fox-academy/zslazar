3096fc883fdbb26d68b5c187ec75b5c48f3e09ce
zslazar <zs.lazar@gmail.com>
2017-11-28 00:01:13 +0100

    exercise-14 done

---
+++ week-02/day-01/Exercise-14/Exercise-14/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+    namespace GreenFox
+{
+        class Program
+    {
+            static void Main(string[] args)
+        {
+
+            // Modify this program to greet user instead of the World!
+            // The program should ask for the name of the user
+
+            Console.WriteLine("Hello user what is your name?");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Hello, " + name);
+            
+            Console.ReadLine();
+
+        }
+    }
+}
