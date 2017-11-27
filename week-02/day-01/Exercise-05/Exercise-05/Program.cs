80e8c283241d893fdba0b009e6a26ef422d0222b
zslazar <zs.lazar@gmail.com>
2017-11-27 17:42:16 +0100

    exercise05-06

---
+++ week-02/day-01/Exercise-05/Exercise-05/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number0 = 13;
+            int number1 = 22;
+
+            Console.WriteLine(number0 + number1);
+            Console.WriteLine(number0 - number1);
+            Console.WriteLine(number0 * number1);
+            Console.WriteLine((double)number1 / number0);
+            Console.WriteLine(number1 / number0);
+            Console.WriteLine(number1 % number0);
+            
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ week-02/day-01/Exercise-06/Exercise-06/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Exercise_06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // An average Green Fox attendee codes 6 hours daily
+            // The semester is 17 weeks long
+            // Print how many hours is spent with coding in a semester by an attendee,
+            // if the attendee only codes on workdays.
+            //5*6/52
+
+            int code = 6;
+            int week = 17;
+         
+            int value = 5 * code * week;
+            
+            Console.WriteLine("workhours sum: " + value);
+
+            // Print the percentage of the coding hours in the semester if the average work hours weekly is 52
+            //6*5/52*100
+
+            int workhour = 52;
+
+            Console.WriteLine("percentage: " + (double)code * 5 / 52 * 100);
+
+            Console.ReadLine();
+        }
+    }
+}
