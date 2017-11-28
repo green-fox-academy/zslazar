3cee2cd421a6e58575a665b5ce057472f9548703
zslazar <zs.lazar@gmail.com>
2017-11-28 15:44:26 +0100

    day01exercise08

---
+++ week-02/day-01/Exercise08Swap/Exercise08Swap/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Exercise08Swap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // a 2 ertek
+            int a = 123;
+            int b = 526;
+
+            // uj valtozo hozzaadva
+            int c;
+
+            // ertekek felcserelese
+            c = a;
+            a = b;
+            b = c;
+
+            // ertekek kiirasa
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.ReadLine();
+        }
+    }
+}
