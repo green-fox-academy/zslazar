5de43bae5c9c3b4a128c5da4c6906769ac62475b
zslazar <zs.lazar@gmail.com>
2017-11-28 14:44:26 +0100

    Exercise04

---
+++ week-02/day-02/Functions-04-Summing/Functions-04-Summing/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        // - Write a function called `sum` that sum all the numbers
+        //   until the given parameter
+        //1. egy szamot beolvastatni !szamkent
+        //2. return - osszeadani 0tol az adott szamig
+
+        //Console.WriteLine();
+        //Console.ReadLine();
+        {
+
+            //keri az erteket es kezeli szamkent
+            Console.WriteLine("give me a number");
+            string number = Console.ReadLine();
+            int addnumber = int.Parse(number);
+            
+            //az eredmeny megjelenitese
+            Console.WriteLine(Sum(addnumber));
+            Console.ReadLine();
+        }
+            //a muvelet definialasa
+        static int Sum(int num)
+        {
+            return (num * (num + 1)) / 2;
+        }
+
+        }
+}
