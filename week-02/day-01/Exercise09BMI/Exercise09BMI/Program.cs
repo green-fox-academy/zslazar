4c6d2ba8fe56b38a079c46511bd26b6fa4b571e2
zslazar <zs.lazar@gmail.com>
2017-11-28 16:07:58 +0100

    day01exercise09

---
+++ week-02/day-01/Exercise09BMI/Exercise09BMI/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Print the Body mass index (BMI) based on these values = weight/height
+            double massInKg = 81.2;
+            double heightInM = 1.78;
+
+            double mxh = massInKg / heightInM;
+
+            Console.WriteLine(mxh / heightInM);
+
+            Console.ReadLine();
+        }
+    }
+}
