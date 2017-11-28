0bb509a2a2b41bb95525f765a4192846e40d0775
zslazar <zs.lazar@gmail.com>
2017-11-28 10:00:15 +0100

    Exercise15 draft

---
+++ week-02/day-01/Exercise-16/Exercise-16/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that asks for two integers
+            // The first represents the number of chickens the farmer has
+            // The second represents the number of pigs the farmer has
+
+            Console.WriteLine("Howdy, number of chickens?");
+            string chicken = Console.ReadLine();
+            int chickenleg = int.Parse(chicken);
+
+            Console.WriteLine("Nice, and number of pigs?");
+            string pig = Console.ReadLine();
+            int pigleg = int.Parse(pig);
+
+            Console.WriteLine("Allnumber, " + (chickenleg * 2) + (pigleg * 4));
+
+            Console.ReadLine();
+
+            // It should print how many legs all the animals have
+        }
+    }
+}
