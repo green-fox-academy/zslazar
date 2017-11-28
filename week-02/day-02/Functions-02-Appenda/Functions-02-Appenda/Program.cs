67ad5a9b88b1a924584e3500d17523ab36be7173
zslazar <zs.lazar@gmail.com>
2017-11-28 12:23:11 +0100

    Exercise03 done

---
+++ week-02/day-02/Functions-02-Appenda/Functions-02-Appenda/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Example
+{
+    class Program
+
+    // - Create a string variable named `am` and assign the value `kuty` to it
+    // - Write a function called `appendA` that gets a string as an input
+    //   and appends an 'a' character to its end
+    
+    {
+        static void Main(string[] args)
+        {
+            string am = "kuty";
+            appendA(am);
+        }
+
+    // - Print the result of `appendA(am)`
+
+        static void appendA(string adda)
+        {
+            Console.WriteLine(adda + "a");
+            Console.ReadLine();
+        }
+    }
+}
