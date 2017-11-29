324a3c96cd81299dac3ec8a545b5acd61e90b7d3
zslazar <zs.lazar@gmail.com>
2017-11-29 12:16:47 +0100

    exercise 01 done

---
+++ week-02/day-03/Strings01/Strings01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SimpleReplace
+{
+    public class SimpleReplace
+    {
+        public static void Main(string[] args)
+        {
+            string example = "In a dishwasher far far away";
+
+            //Console.WriteLine(sampleString1.Replace("some","apple"));
+            
+            // I would like to replace "dishwasher" with "galaxy" in this example, but it has a problem.
+            // Please fix it for me!
+            // Expected ouput: In a galaxy far far away
+
+            Console.WriteLine(example.Replace("dishwasher", "galaxy"));
+            Console.ReadLine();
+        }
+    }
+}
