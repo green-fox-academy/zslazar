6e11dad974ee2aa77463af2f7cbb4dcbc453bc5c
zslazar <zs.lazar@gmail.com>
2017-11-29 13:15:47 +0100

    exercise 02 done

--- week-02/day-03/Strings02/Strings02/Program.cs
+++ week-02/day-03/Strings02/Strings02/Program.cs
@@ -11,16 +11,21 @@ namespace UrlFixer
         {
             string url = "https//www.reddit.com/r/nevertellmethebots";
 
-            
+
             // Accidentally I got the wrong URL for a funny subreddit. It's probably "odds" and not "bots"
             // Also, the URL is missing a crutial component, find out what it is and insert it too!
             // Try to solve it more than once using different string functions!
 
             ////Console.WriteLine(sampleString1.Replace("some","apple"));
+
+            //first solution
             //url = url.Replace("bots", "odds");
             //url = url.Insert(5, ":");
 
-            Console.WriteLine();
+            //second solution
+            url = url.Replace("bots", "odds").Insert(5, ":");
+
+            Console.WriteLine(url);
             Console.ReadLine();
         }
     }
