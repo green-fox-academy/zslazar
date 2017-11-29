a59d0429c51a85c417e271a60691650c5e1f1b1f
zslazar <zs.lazar@gmail.com>
2017-11-29 16:46:51 +0100

    exercise 06 done

---
+++ week-02/day-03/Lists01SolarSystem/Lists01SolarSystem/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SolarSystem
+{
+    public class SolarSystem
+    {
+        public static void Main(string[] args)
+        {
+            var planetList = new List<string> { "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Uranus", "Neptune" };
+
+            planetList.Insert(5, "Saturn");
+            // Saturn is missing from the planetList
+            // Insert it into the correct position
+
+            foreach (string planets in planetList)
+            {
+                Console.WriteLine(planets);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
