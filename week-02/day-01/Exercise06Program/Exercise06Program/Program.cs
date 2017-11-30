db8d4b670cc334a85dd66471dc3b3ff15f8d3270
zslazar <zs.lazar@gmail.com>
2017-11-30 11:01:07 +0100

    day 01 exercise 06 again

---
+++ week-02/day-01/Exercise06Program/Exercise06Program/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int h = 17;
+            int n = 5;
+            int o = 6;
+
+            int s = (h * n * o) / h;
+
+            int av = 52;
+
+            double p = ((double)o * n / av) * 100;
+
+            Console.WriteLine("percentage: " + p);
+
+            Console.ReadLine();
+        }
+    }
+}
