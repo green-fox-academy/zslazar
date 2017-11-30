40a05691073c2678d5a63c2bdd4c58bec5fc56f0
zslazar <zs.lazar@gmail.com>
2017-11-30 16:43:56 +0100

    day 01 exercise 20 refact

--- week-02/day-01/Exercise20PrintBigger/Exercise20PrintBigger/Program.cs
+++ week-02/day-01/Exercise20PrintBigger/Exercise20PrintBigger/Program.cs
@@ -9,12 +9,16 @@ namespace Exercise20PrintBigger
             // Write a program that asks for two numbers and prints the bigger one
 
             Console.WriteLine("first number?");
-            string number1 = Console.ReadLine();
-            int number01 = int.Parse(number1);
+            //string number1 = Console.ReadLine();
+            //int number01 = int.Parse(number1);
+
+            int number01 = Int32.Parse(Console.ReadLine());
 
             Console.WriteLine("second number?");
-            string number2 = Console.ReadLine();
-            int number02 = int.Parse(number2);
+            //string number2 = Console.ReadLine();
+            //int number02 = int.Parse(number2);
+
+            int number02 = Int32.Parse(Console.ReadLine());
 
             if (number01 > number02)
             {
