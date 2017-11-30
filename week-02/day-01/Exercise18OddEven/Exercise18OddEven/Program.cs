5888bcf0e2728df35897a3828a3e704e565fece1
zslazar <zs.lazar@gmail.com>
2017-11-30 14:02:41 +0100

    day 01 exercise 18 done

---
+++ week-02/day-01/Exercise18OddEven/Exercise18OddEven/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Exercise18OddEven
+{
+    class Program
+
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that reads a number from the standard input,
+            // Then prints "Odd" if the number is odd, or "Even" if it is even.
+
+            Console.WriteLine("Give me a number");
+            string number = Console.ReadLine();
+            int numbers = int.Parse(number);
+
+            //i % 2 == 0
+            
+            if (numbers % 2 == 1)
+            {
+                Console.WriteLine("Even");
+            }
+        
+
+            else 
+            {
+                Console.WriteLine("Odd");
+            }
+            Console.ReadLine();
+        }
+    }
+}
