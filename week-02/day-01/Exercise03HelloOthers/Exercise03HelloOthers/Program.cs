8da2859cf154684edf53a9ca7381ddec15e6c5ef
zslazar <zs.lazar@gmail.com>
2017-11-30 10:03:25 +0100

    day 01 exercise 03 done

---
+++ week-02/day-01/Exercise03HelloOthers/Exercise03HelloOthers/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Exercise03HelloOthers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string hello = "Hello, Esther!\nHello, Mary!\nHello, Joe!";
+            Console.WriteLine(hello);
+            
+            Console.ReadLine();
+        }
+    }
+}
