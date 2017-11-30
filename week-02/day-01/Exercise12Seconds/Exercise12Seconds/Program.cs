53bf69a29e4150625dede3bdb1a60e190ba6ed3f
zslazar <zs.lazar@gmail.com>
2017-11-30 12:28:45 +0100

    day 01 exercise 13 done

---
+++ week-02/day-01/Exercise12Seconds/Exercise12Seconds/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Exercise12Seconds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int currentHours = 14;
+            int currentMinutes = 34;
+            int currentSeconds = 42;
+
+            // Write a program that prints the remaining seconds (as an integer) from a
+            // day if the current time is represented bt the variables
+
+            int secondsnow = currentHours * 3600 + currentMinutes * 60 + currentSeconds;
+
+            int allseconds = 86400;
+
+            int remain = (allseconds - secondsnow);
+
+            Console.WriteLine("Remain: " + remain + " seconds");
+            Console.ReadLine();
+        }
+    }
+}
