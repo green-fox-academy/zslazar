c59e0bad80b37f22de404568e5d1ed7e64ed50cd
zslazar <zs.lazar@gmail.com>
2017-11-30 21:36:17 +0100

    exercise 23 done

---
+++ week-02/day-01/23IwontCheat/23IwontCheat/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _23IwontCheat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Create a program that writes this line 100 times:
+            // "I won't cheat on the exam!"
+
+            string text = "I won't cheat on the exam!";
+            for (int i = 0; i <= 99; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
