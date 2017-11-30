04a420ab0084d9a889fbc8509cf5588dcefe4019
zslazar <zs.lazar@gmail.com>
2017-11-30 11:30:07 +0100

    day 01 exercise 09 again

--- week-02/day-01/Exercise09BMI/Exercise09BMI/Program.cs
+++ week-02/day-01/Exercise09BMI/Exercise09BMI/Program.cs
@@ -10,10 +10,14 @@ namespace GreenFox
             double massInKg = 81.2;
             double heightInM = 1.78;
 
-            double mxh = massInKg / heightInM;
+            //solution 1
+            //double mxh = massInKg / heightInM;
+            //Console.WriteLine(mxh / heightInM);
 
-            Console.WriteLine(mxh / heightInM);
+            //solution2
+            double BMI = massInKg / Math.Pow(heightInM, 2);
 
+            Console.WriteLine("BMI index is " + BMI);
             Console.ReadLine();
         }
     }
