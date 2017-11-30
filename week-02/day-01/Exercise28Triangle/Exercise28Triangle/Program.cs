7d2183e71f50ab71ac56eecdac4f9d04ee18b43a
zslazar <zs.lazar@gmail.com>
2017-11-30 23:06:02 +0100

    exercise 28 done

---
+++ week-02/day-01/Exercise28Triangle/Exercise28Triangle/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Exercise28Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that reads a number from the standard input, then draws a
+            // triangle like this:
+            // *
+            // **
+            // ***
+            // ****
+            //
+            // The triangle should have as many lines as the number was
+
+            int number = int.Parse(Console.ReadLine());
+            string star = "*";
+            
+
+            for (int i = 0; i < number; i++)
+            {
+                Console.WriteLine(star);
+                star += "*";
+            }
+            Console.ReadLine();
+        }
+    }
+}
