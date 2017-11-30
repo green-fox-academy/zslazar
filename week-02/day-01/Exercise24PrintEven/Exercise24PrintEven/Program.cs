0bfc59a0dfc3e9ad8d6d5a8f3e7a67a6207ee2bb
zslazar <zs.lazar@gmail.com>
2017-11-30 22:08:30 +0100

    exercise 24 done

---
+++ week-02/day-01/Exercise24PrintEven/Exercise24PrintEven/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Exercise24PrintEven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a program that prints all the even numbers between 0 and 500
+
+            //solution 1
+            // for (int i = 1; i < 251; i++)
+            //
+            //     Console.WriteLine(i*2);
+            // Console.ReadLine();
+
+            //solution 2
+            //int i = 0;
+            //while (i < 500)
+            //{
+            //    i += 2;
+            //    Console.WriteLine(i);
+            //}
+            //Console.ReadLine();
+
+            //solution 3
+            
+            for (int i = 0; i <= 500; i = i + 2)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+                       
+
+
+        }
+    }
+}
