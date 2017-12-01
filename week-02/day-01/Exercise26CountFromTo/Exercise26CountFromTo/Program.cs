ccd8401126a65588937688d4075cb2aa003876f0
zslazar <zs.lazar@gmail.com>
2017-12-01 11:24:56 +0100

    exercise 26 done

---
+++ week-02/day-01/Exercise26CountFromTo/Exercise26CountFromTo/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Exercise26CountFromTo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a program that asks for two numbers
+            // If the second number is not bigger than the first one it should print:
+            // "The second number should be bigger"
+            //
+            // If it is bigger it should count from the first number to the second by one
+            //
+            // example:
+            //
+            // first number: 3, second number: 6, should print:
+            //
+            // 3
+            // 4
+            // 5
+            Console.WriteLine("give me a number");
+            int number01 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("give me another number");
+            int number02 = int.Parse(Console.ReadLine());
+
+            if (number01 > number02)
+            {
+                Console.WriteLine("The second number should be bigger");
+            }
+
+            else
+            {
+                for (int i = number01; i < number02; i++)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
