dcd9b7fdaa022db18e55d682255de5f0ae9e2761
zslazar <zs.lazar@gmail.com>
2017-12-01 14:14:10 +0100

    exercise 01 new solution

---
+++ week-02/day-01/Functions-01-Doubling-again/Functions-01-Doubling-again/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Functions_01_Doubling_again
+{
+    class Program
+    {
+        // - Create an integer variable named `ak` and assign the value `123` to it
+        // - Create a function called `doubling` that doubles it's input parameter
+        // - Print the result of `doubling(ak)`
+
+        public static void Doubling()
+        {
+            int ak = 123;
+            Console.WriteLine(ak * 2);
+        }
+
+        static void Main(string[] args)
+        {
+            Doubling();
+            Console.ReadLine();
+        }
+    }
+}
