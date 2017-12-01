fa1a170d98282f14740c10e2423d52c54698220d
zslazar <zs.lazar@gmail.com>
2017-12-01 14:26:51 +0100

    exercise 02 new solution

---
+++ week-02/day-02/Functions-02-Greeter-again/Functions-02-Greeter-again/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Functions_02_Greeter_again
+{
+    class Program
+    {
+
+        // - Create a string variable named `al` and assign the value `Greenfox` to it
+        // - Create a function called `greet` that greets it's input parameter
+        // - Greeting is printing e.g. `Greetings dear, Greenfox`
+        // - Greet `al`
+
+        public static void Greet()
+        {
+            string al = "Greenfox";
+            Console.WriteLine("Greetings dear, " + al);
+
+        }
+
+        static void Main(string[] args)
+        {
+            Greet();
+            Console.ReadLine();
+        }
+    }
+}
+
