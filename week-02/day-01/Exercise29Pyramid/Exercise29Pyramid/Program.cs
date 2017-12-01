320500d87b84e997798aeeb03d031c4923844511
zslazar <zs.lazar@gmail.com>
2017-12-01 13:52:32 +0100

    exercise 29 done

---
+++ week-02/day-01/Exercise29Pyramid/Exercise29Pyramid/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Exercise29Pyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that reads a number from the standard input, then draws a
+            // triangle like this:
+            //
+            // *
+            // **
+            // ***
+            // ****
+            //
+            // The triangle should have as many lines as the number was
+
+            int number = int.Parse(Console.ReadLine());
+            string star = "*";
+            int i;
+            int j;
+
+
+
+            for (i = 0; i < number; i++)
+            {
+                for (j = i; j < number - 1; j++)
+                {
+                    Console.Write(" ");
+                }
+
+                Console.WriteLine(star);
+                star += "**";
+            }
+
+            Console.ReadKey();
+        }
+
+    }
+}
