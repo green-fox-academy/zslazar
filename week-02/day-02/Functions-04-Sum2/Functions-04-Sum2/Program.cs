b0e5b675ad85cd0b7eae0c8d80847a36b6a79b0f
zslazar <zs.lazar@gmail.com>
2017-12-02 23:16:51 +0100

    exercise 04 again

---
+++ week-02/day-02/Functions-04-Sum2/Functions-04-Sum2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Example
+{
+    class Program
+    {
+        // - Write a function called `sum` that sum all the numbers
+        //   until the given parameter
+
+        public static void Sum()
+        {
+            int number = int.Parse(Console.ReadLine());
+            number = number * (number + 1) / 2;
+            Console.WriteLine(number);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("give me a number");
+            Sum();
+            Console.ReadLine();
+        }
+
+    }
+}
--- week-02/day-02/Functions-04-Summing/Functions-04-Summing/Program.cs
+++ week-02/day-02/Functions-04-Summing/Functions-04-Summing/Program.cs
@@ -4,31 +4,26 @@ namespace Example
 {
     class Program
     {
-        static void Main(string[] args)
-
-        // - Write a function called `sum` that sum all the numbers
-        //   until the given parameter
-        //1. egy szamot beolvastatni !szamkent
-        //2. return - osszeadani 0tol az adott szamig
-
-        //Console.WriteLine();
-        //Console.ReadLine();
-        {
-
-            //keri az erteket es kezeli szamkent
-            Console.WriteLine("give me a number");
-            string number = Console.ReadLine();
-            int addnumber = int.Parse(number);
-            
-            //az eredmeny megjelenitese
-            Console.WriteLine(Sum(addnumber));
-            Console.ReadLine();
-        }
             //a muvelet definialasa
         static int Sum(int num)
         {
             return (num * (num + 1)) / 2;
         }
 
+        static void Main(string[] args)
+
+        // - Write a function called `sum` that sum all the numbers
+        //   until the given parameter
+        
+        {
+            //keri az erteket es kezeli szamkent
+            Console.WriteLine("give me a number");
+            int addnumber = int.Parse(Console.ReadLine());
+            
+            //az eredmeny megjelenitese
+            Console.WriteLine(Sum(addnumber));
+            Console.ReadLine();
         }
+
+    }
 }
