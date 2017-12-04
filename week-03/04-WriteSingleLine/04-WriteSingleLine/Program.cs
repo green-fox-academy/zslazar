37c69a64f09b600696be1d362d450ce484495ff7
zslazar <zs.lazar@gmail.com>
2017-12-04 15:22:09 +0100

    exercise 04 done

---
+++ week-03/04-WriteSingleLine/04-WriteSingleLine/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace _04_WriteSingleLine
+{
+    class Program
+    // Open a file called "my-file.txt"
+    // Write your name in it as a single line
+    // If the program is unable to write the file,
+    // then it should print an error message like: "Unable to write file: my-file.txt"
+
+
+    {
+        static void Main(string[] args)
+        {
+            string path = @"c:\greenfox\zslazar\week-03\my-file.txt";
+            using (StreamWriter writer = new StreamWriter(path))
+            try
+                {
+                string name = "Zsuzsa Lazar";
+                writer.WriteLine(name);
+                }
+
+            catch
+                {
+                    string errormsg = "Unable to write file: my-file.txt";
+                    Console.WriteLine(errormsg);
+                }
+
+                finally
+                {
+                    Console.ReadLine();
+                }
+        }
+    }
+}
