43a80fce9a0e14ab1ede634b0b694f0466adb4e0
zslazar <zs.lazar@gmail.com>
2017-12-04 11:37:37 +0100

    Material Review

---
+++ week-03/Practice/Practice/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //beolvassa a tartalmat
+            //string[] array = File.ReadAllLines(@"C:\greenfox\zslazar\week-03\text.txt");
+
+            //foreach (string line in array)
+            //{
+            //    Console.WriteLine(line);
+            //}
+
+            //Console.ReadLine();
+
+            //vagy igy is lehet, eredmeny ugyanaz
+            //string sometext = File.ReadAllText(@"C:\greenfox\zslazar\week-03\text.txt");
+            //Console.WriteLine(sometext);
+            //Console.ReadLine();
+
+
+            //kiiratni a tartalmat
+            //felulirja a bennelevot ha nem ures
+            //File.WriteAllText(@"C:\greenfox\zslazar\week-03\text.txt", "Fifa is a good game");
+            //Console.ReadLine();
+
+            //string[] array = { "123", "EA" };
+            //File.WriteAllText(@"C:\greenfox\zslazar\week-03\text.txt", array);
+
+            //Console.ReadLine();
+
+            //StreamReader sr = new StreamReader(@"C:\greenfox\zslazar\week-03\text.txt");
+            //string line = "";
+            //while (sr.ReadLine() != null)
+            //{
+            //    Console.WriteLine(sr);
+            //}
+
+            //while (line != null)
+            //{
+            //line = sr.ReadLine();
+            //Console.WriteLine(line);
+            //}
+
+            //StreamWriter sw = new StreamWriter(@"C:\greenfox\zslazar\week-03\text.txt", true);
+            // hozzafuzi a meglevo tartalomhoz
+
+            //StreamWriter sw = new StreamWriter(@"C:\greenfox\zslazar\week-03\text.txt");
+            //string line1 = "Fifa";
+
+            //sw.WriteLine(line1);
+
+            //ugyanaz mint sw.Dispose(); csak dispose kitorli a memoriabol 
+            //sw.Close();
+
+        }
+    }
+}
+
---
+++ week-03/Practice02/Practice02/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+
+namespace Practice02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 3;
+            double b = 0;
+
+            string number = "500";
+
+            double num = int.Parse(number);
+
+
+            
+            try
+            {
+                Console.WriteLine(a / b);
+
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+
+        }
+        //sajat kiveteluyenet letrehozasa
+        //public class MyException : Exception
+        //{
+        //    public MyException
+        //        {
+        //        Console.WriteLine("Please ezt ne");
+        //        }
+
+
+        
+    }
+}
