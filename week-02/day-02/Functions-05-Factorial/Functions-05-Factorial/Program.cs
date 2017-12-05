fdac7eb2d68560b3d999fda81d66bb42ea59b3aa
zslazar <zs.lazar@gmail.com>
2017-12-05 23:43:51 +0100

    exercise 05 done

---
+++ week-02/day-02/Functions-05-Factorial/Functions-05-Factorial/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Functions_05_Factorial
+{
+    class Program
+    // - Create a function called `factorio`
+    //   that returns it's input's factorial
+    {
+        public static void Factorio(int input)
+        {
+            int fact = 1;
+            for (int i = 1; i <= input; i++)
+            {
+                fact *= i;
+            }
+            Console.WriteLine(fact);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("give me a number");
+            int input = int.Parse(Console.ReadLine());
+            Factorio(input);
+
+            Console.ReadLine();
+        }
+    }
+}
