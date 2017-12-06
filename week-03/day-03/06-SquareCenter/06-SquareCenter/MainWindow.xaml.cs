47432013ccf343a389a306a45d31611cab6a3f49
zslazar <zs.lazar@gmail.com>
2017-12-06 14:37:13 +0100

    exercise 06

---
+++ week-03/day-03/06-SquareCenter/06-SquareCenter/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GreenFox;
+
+namespace _06_SquareCenter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            DrawRectangle(foxDraw);
+        }
+
+        public static void DrawRectangle(FoxDraw foxDraw)
+        {
+            foxDraw.DrawRectangle(258, 170, 10, 10);
+        }
+    }
+}
