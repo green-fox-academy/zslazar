7b83fa35eb5a37487ff8efda754acca4d1a6a860
zslazar <zs.lazar@gmail.com>
2017-12-06 16:00:40 +0100

    exercise 08 done

---
+++ week-03/day-03/08-SquarePosition/08-SquarePosition/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GreenFox;
+
+namespace _08_SquarePosition
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            DrawSquares(foxDraw);
+            // create a square drawing function that takes 2 parameters:
+            // the x and y coordinates of the square's top left corner
+            // and draws a 50x50 square from that point.
+            // draw 3 squares with that function. 
+
+        }
+
+        public static void DrawSquares(FoxDraw foxDraw)
+        {
+
+            foxDraw.FillColor(Colors.AliceBlue);
+
+            for (int i = 0; i < 3; i++)
+            {
+                foxDraw.DrawRectangle(i*50, i*50, 50, 50);
+            }
+
+        }
+
+
+    }
+}
