c78d0bf22e4bf2d79e283ca9f1c454499050fc89
zslazar <zs.lazar@gmail.com>
2017-12-06 16:57:05 +0100

    exercise 09 done

---
+++ week-03/day-03/09-CenteredBoxes/09-CenteredBoxes/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GreenFox;
+
+namespace _09_CenteredBoxes
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            double Size = 200;
+            CenterSquares(foxDraw, Size);
+
+            // create a square drawing function that takes 1 parameter:
+            // the square size
+            // and draws a square of that size to the center of the canvas.
+            // draw 3 squares with that function.
+
+        }
+
+        public static void CenterSquares(FoxDraw foxDraw, double Size)
+        {
+                        
+            for (int i = 1; i < 4; i++)
+            {
+                foxDraw.DrawRectangle(200 - Size/2, 200 - Size/2, Size, Size);
+                Size = Size - 50;
+            }
+        }
+
+    }
+}
