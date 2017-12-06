4df4bb5498831c1040e7ef6bc09ff85158384089
zslazar <zs.lazar@gmail.com>
2017-12-06 11:44:17 +0100

    exercise 03 done

---
+++ week-03/day-03/03-Diagonals/03-Diagonals/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GreenFox;
+
+namespace _03_Diagonals
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            DrawGreenDiagonals(foxDraw);
+            // draw the canvas' diagonals in green.
+        }
+
+        public static void DrawGreenDiagonals(FoxDraw foxDraw)
+        {
+            foxDraw.StrokeColor(Colors.Green);
+            foxDraw.DrawLine(0, 0, 525, 350);
+            foxDraw.DrawLine(0, 350, 525, 0);
+
+        }
+    }
+}
