e1d09d3ba465f3ad1307ee7038a2e4d678599a5c
zslazar <zs.lazar@gmail.com>
2017-12-07 14:21:30 +0100

    exercise 04 done

---
+++ week-03/day-04/04-Power/04-Power/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Power
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 4;
+            int exponent = 6;
+            Console.WriteLine(Power(number, exponent));
+            Console.ReadLine();
+        }
+
+        static int Power(int number, int exponent)
+        {
+            if (exponent == 0)
+            {
+                return 1;
+            }
+            else
+            {
+            return Power(number, exponent - 1) * number;
+
+            }
+        }
+    }
+}
