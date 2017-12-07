c2ede4cd05cc3a8ca233c176a6c2a07229b03a07
zslazar <zs.lazar@gmail.com>
2017-12-07 11:26:15 +0100

    exercise 01

---
+++ week-03/day-04/01-Counter/01-Counter/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _01_Counter
+{
+    // Write a recursive function that takes one parameter: n and counts down from n.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            Counter(a);
+            Console.ReadLine();
+        }
+
+        public static int Counter(int n)
+        {
+            if (n > 0)
+            {
+                Console.WriteLine(n);
+                return Counter(n - 1);
+            }
+            else
+            {
+                return n;
+            }
+        }
+    }
+}
