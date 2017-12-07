9c0a01c3eb7ce713a5bff271a1c555a07a55096a
zslazar <zs.lazar@gmail.com>
2017-12-07 16:41:38 +0100

    exercise 06 done

---
+++ week-03/day-04/06-Bunnies02/06-Bunnies02/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _06_Bunnies02
+{
+    // We have bunnies standing in a line, numbered 1, 2, ... The odd bunnies
+    // (1, 3, ..) have the normal 2 ears. The even bunnies (2, 4, ..) we'll say
+    // have 3 ears, because they each have a raised foot. Recursively return the
+    // number of "ears" in the bunny line 1, 2, ... n (without loops or multiplication).
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ear = 2;
+            int freakEar = ear + 1;
+            int bunnies = 20;
+            Console.WriteLine(CountEar(bunnies, ear, freakEar));
+            Console.ReadLine();
+        }
+
+        public static int CountEar(int bunnies, int ear, int freakEar)
+        {
+            if (bunnies == 0)
+            {
+                return 0;
+            }
+            else if (bunnies == 1)
+            {
+                return ear;
+            }
+            else if (bunnies % 2 == 0)
+            {
+                return freakEar + CountEar(bunnies - 1, ear, freakEar);
+            }
+            else 
+            {
+                return ear + CountEar(bunnies - 1, ear, freakEar);
+            }
+            
+        }
+    }
+}
