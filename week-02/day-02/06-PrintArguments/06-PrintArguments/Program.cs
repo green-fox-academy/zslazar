5f4bc08f892895f84d5ecb7e662b1fda3876c880
zslazar <zs.lazar@gmail.com>
2017-12-09 23:18:00 +0100

    exercise 06 done

---
+++ week-02/day-02/06-PrintArguments/06-PrintArguments/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _06_PrintArguments
+{
+    class Program
+    {
+        // - Create a function called `printer`which prints the input String parameters
+        // - It can have any number of parameters
+
+        // Examples
+        // printer("first")
+        // printer("first", "second")
+        // printer("first", "second", "third", "fourth")
+        
+        static void Main(string[] args)
+        {
+            string[] array = {"first", "second", "third", "fourth" };
+            Printer(array);
+            Console.ReadLine();
+        }
+
+        public static void Printer(string[] text)
+        {                
+            for (int i = 0; i < text.Length; i++)
+            {
+                Console.WriteLine(text[i]);
+            }
+        }
+    }
+}
