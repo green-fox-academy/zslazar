bd216ca7c386002c726718358235e2ec99ef023a
zslazar <zs.lazar@gmail.com>
2017-12-09 23:32:29 +0100

    exercise 07 done

---
+++ week-02/day-02/07-PrintThird/07-PrintThird/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _07_PrintThird
+{
+    class Program
+    {
+        // - Create an array variable named `q`
+        //   with the following content: `[4, 5, 6, 7]`
+        // - Print the third element of `q`
+        static void Main(string[] args)
+        {
+            //solution01
+            int[] q = {4, 5, 6, 7};
+            Console.WriteLine(q[2]);
+            Console.ReadLine();
+
+//solution02 - from here, down
+        //    int[] q = { 4, 5, 6, 7 };
+        //    Printer(q[2]);
+        //    Console.ReadLine();
+        //}
+
+        //public static void Printer(int q)
+        //{
+            
+        //    Console.WriteLine(q);
+        }
+
+    }
+}
