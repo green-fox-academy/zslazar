ea2b61a8891a08c13a9600870d0d17bf5ef81e07
zslazar <zs.lazar@gmail.com>
2017-12-10 23:32:10 +0100

    exercise 16 - array method return

---
+++ week-02/day-02/16-AppendA02/16-AppendA02/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _16_AppendA02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nimals = { "kuty", "macsk", "cic" };
+            for (int i = 0; i < nimals.Length; i++)
+            Console.WriteLine(AppendA(nimals[i]));
+            Console.ReadLine();
+        }
+
+        static string AppendA (string word)
+        {
+            return word + "a";
+        }
+    }
+}
