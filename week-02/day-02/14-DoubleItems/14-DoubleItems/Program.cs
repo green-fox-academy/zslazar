5c3181efb7db71dab6218f3075378b6681a2f97c
zslazar <zs.lazar@gmail.com>
2017-12-10 23:14:09 +0100

    exercise 14 done

---
+++ week-02/day-02/14-DoubleItems/14-DoubleItems/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _14_DoubleItems
+{
+    class Program
+    {
+        // - Create an array variable named `ag`
+        //   with the following content: `[3, 4, 5, 6, 7]`
+        // - Double all the values in the array
+        static void Main(string[] args)
+        {
+            int[] ag = { 3, 4, 5, 6, 7 };
+            for (int i = 0; i < ag.Length; i++)
+            {
+                //solution1
+                ag[i] = ag[i] * 2;
+                //solution2
+                //ag[i] = ag[i] + ag[i];
+                Console.WriteLine(ag[i]); 
+            }
+            Console.ReadLine();
+        }
+    }
+}
