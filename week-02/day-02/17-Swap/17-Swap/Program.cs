fd988239d279bef941c11d7b547e364ad968bb6c
zslazar <zs.lazar@gmail.com>
2017-12-10 23:46:01 +0100

    exercise 17 - array swap elements

---
+++ week-02/day-02/17-Swap/17-Swap/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _17_Swap
+{
+    class Program
+    {
+        // - Create an array variable named `abc`
+        //   with the following content: `["first", "second", "third"]`
+        // - Swap the first and the third element of `abc`
+        static void Main(string[] args)
+        {
+            string[] abc = { "first", "second", "third" };
+            string swap = abc[0];
+            abc[0] = abc[2];
+            abc[2] = abc[0];
+
+            for (int i = 0; i < abc.Length; i++)
+            {
+                Console.WriteLine(abc[i]);
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
