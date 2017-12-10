377ec7a64d138d46f69dba38ca135f5d85cfee4b
zslazar <zs.lazar@gmail.com>
2017-12-10 22:42:28 +0100

    exercise 12 done

---
+++ week-02/day-02/12-IncrementElement/12-IncrementElement/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _12_IncrementElement
+{
+    class Program
+    {
+        //- Create an array variable named `t`
+        //  with the following content: `[1, 2, 3, 4, 5]`
+        //- Increment the third element
+        //- Print the third element
+        static void Main(string[] args)
+        {
+            int[] t = { 1, 2, 3, 4, 5 };
+            t[2] = t[2] + 1;
+            Console.WriteLine(t[2]);
+            Console.ReadLine();
+        }
+    }
+}
