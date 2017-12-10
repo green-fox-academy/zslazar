1866aa961363f70765a7810f9639ac3a358fb38b
zslazar <zs.lazar@gmail.com>
2017-12-11 00:15:16 +0100

    exercise 05 - string reverse

---
+++ week-02/day-03/Strings05Reverse02/Strings05Reverse02/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Strings05Reverse02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a function that can reverse a String, which is passed as the parameter
+            string reversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI";
+                 
+            char[] reversedarray = reversed.ToCharArray();
+            Array.Reverse(reversedarray);
+            string output = new string(reversedarray);
+
+            Console.WriteLine(output);
+            Console.ReadLine();
+        }
+    }
+}
