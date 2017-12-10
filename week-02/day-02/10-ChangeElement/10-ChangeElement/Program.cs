4af3175dfea8f847eeb4f31c6869f37b55088e03
zslazar <zs.lazar@gmail.com>
2017-12-10 17:04:22 +0100

    exercise 10 done

---
+++ week-02/day-02/10-ChangeElement/10-ChangeElement/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _10_ChangeElement
+{
+    class Program
+    {
+        // - Create an array variable named `s` with the following content: `[1, 2, 3, 8, 5, 6]`
+        // - Change the 8 to 4
+        // - Print the fourth element
+        static void Main(string[] args)
+        {
+            int[] s = { 1, 2, 3, 8, 5, 6 };
+            s[3] = 4;
+            Console.WriteLine(s[3]);
+            Console.ReadLine();
+        }
+    }
+}
