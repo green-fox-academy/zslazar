4b0fcfa58a4ac4e2a334a57e47fa959d1e35ab66
zslazar <zs.lazar@gmail.com>
2017-12-11 00:04:09 +0100

    exercise 19 - array reverse elements

---
+++ week-02/day-02/19-Reverse/19-Reverse/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _19_Reverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // -Create an array variable named `aj` with the following content: `[3, 4, 5, 6, 7]`
+            // - Reverse the order of the elements in `aj`
+            // - Print the elements of the reversed `aj`
+
+            int[] aj = { 3, 4, 5, 6, 7 };
+            Array.Reverse(aj);
+            for (int i = 0; i < aj.Length; i++)
+            {
+                Console.Write(aj[i] + ", ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
