57e09f51ff644d0422f8ae1606687751bc2eb498
zslazar <zs.lazar@gmail.com>
2017-12-10 22:47:04 +0100

    exercise 13 done

---
+++ week-02/day-02/13-PrintAll/13-PrintAll/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _13_PrintAll
+{
+    class Program
+    {
+        // - Create an array variable named `af`
+        //   with the following content: `[4, 5, 6, 7]`
+        // - Print all the elements of `af`
+        static void Main(string[] args)
+        {
+            int[] af = { 4, 5, 6, 7 };
+            for (int i = 0; i < af.Length; i++)
+                Console.WriteLine(af[i]);
+                Console.ReadLine();
+        }
+    }
+}
