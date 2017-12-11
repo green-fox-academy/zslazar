cdf0856b2e197445811aad39d5607100c9fa9a3a
zslazar <zs.lazar@gmail.com>
2017-12-11 12:29:32 +0100

    exercise 01 - struct, new cs file

---
+++ week-04/day-01/01-PostIt/01-PostIt/PostIt.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01_PostIt
+{
+
+    public struct PostIt
+    {
+        public string BackgroundColor;
+        public string TextColor;
+        public string Text;
+    }
+}
--- week-04/day-01/01-PostIt/01-PostIt/Program.cs
+++ week-04/day-01/01-PostIt/01-PostIt/Program.cs
@@ -6,7 +6,22 @@ namespace _01_PostIt
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            PostIt postIt1;
+            postIt1.BackgroundColor = "orange";
+            postIt1.TextColor = "blue";
+            postIt1.Text = "Idea 1";
+
+            PostIt postIt2;
+            postIt2.BackgroundColor = "pink";
+            postIt2.TextColor = "black";
+            postIt2.Text = "Awesome";
+
+            PostIt postIt3;
+            postIt3.BackgroundColor = "yellow";
+            postIt3.TextColor = "green";
+            postIt3.Text = "Superb";
+            
+            Console.WriteLine();
         }
     }
 }
