da7827f4f092690ddbb1f1b526b48bb1af562369
zslazar <zs.lazar@gmail.com>
2017-12-11 16:37:31 +0100

    exercise 04 roll a dice

---
+++ week-04/day-01/04-Dice/04-Dice/Dice.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _04_Dice
+{
+    //You have a Dice class which has 6 dice
+    //You can roll all of them with Roll()
+    //Check the current rolled numbers with GetCurrent()
+    //You can reroll with Reroll()
+    //Your task is to get where all dice is a 6
+    class Dice
+    {
+        public int valaue;
+        Random random = new Random();
+
+        public int GetCurrent()
+        {
+            return valaue;
+        }
+
+        public void Roll()
+        {
+            valaue = random.Next(1, 7);
+        }
+
+        public void ReRoll()
+        {
+            valaue = random.Next(1, 7);
+        }
+
+    }
+}
---
+++ week-04/day-01/04-Dice/04-Dice/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04_Dice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dices = new List<Dice>();
+            //letrehozok 6 kockat
+            for (int i = 0; i < 6; i++)
+            {
+                dices.Add(new Dice());
+            }
+            //megporgetem
+            for (int j = 0; j < 6; j++)
+            {
+                dices[j].Roll();
+                while (dices[j].GetCurrent() != 0 )
+                {
+                    dices[j].ReRoll();
+                }
+                Console.WriteLine(dices[j].GetCurrent());
+            }
+            Console.ReadLine();                  
+
+        }
+    }
+}
