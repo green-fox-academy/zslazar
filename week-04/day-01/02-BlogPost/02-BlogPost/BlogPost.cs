c5d2891fc6a29a43391746c44d8143ab157c0b8a
zslazar <zs.lazar@gmail.com>
2017-12-11 14:02:01 +0100

    exercise 02 create a class, objets

---
+++ week-04/day-01/02-BlogPost/02-BlogPost/BlogPost.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _02_BlogPost
+{
+    //Create a BlogPost class that has an AuthorName a Title a Text a PublicationDate
+    class BlogPost
+    {
+        public string AuthorName { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public string PublicationDate { get; set; }
+    }
+}
---
+++ week-04/day-01/02-BlogPost/02-BlogPost/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _02_BlogPost
+{
+    class Program
+    {
+        //Create a few blog post objects: "Lorem Ipsum" 
+        //titled by John Doe posted at "2000.05.04." Lorem ipsum dolor sit amet.
+
+        static void Main(string[] args)
+        {
+            var blog1 = new BlogPost();
+            blog1.AuthorName = "John Doe";
+            blog1.Title = "Lorem Ipsum";
+            blog1.Text = "Lorem ipsum dolor sit amet.";
+            blog1.PublicationDate = "2000.05.04.";
+
+            var blog2 = new BlogPost();
+            blog2.AuthorName = "Tim Urban";
+            blog2.Title = "Wait but why";
+            blog2.Text = "A popular long-form, stick-figure-illustrated blog about almost everything.";
+            blog2.PublicationDate = "2010.10.10.";
+
+            var blog3 = new BlogPost();
+            blog3.AuthorName = "William Turton";
+            blog3.Title = "One Engineer Is Trying to Get IBM to Reckon With Trump";
+            blog3.Text = "Daniel Hanley, a cybersecurity engineer at IBM, doesn’t want to be the center of attention. When I asked to take his picture outside one of IBM’s New York City offices, he told me that he wasn’t really into the whole organizer profile thing.";
+            blog3.PublicationDate = "2017.03.28.";
+
+            Console.WriteLine(blog1.Text);
+            Console.ReadLine();
+        }
+    }
+}
