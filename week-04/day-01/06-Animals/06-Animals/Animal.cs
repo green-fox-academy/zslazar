a5c3fcca6fcb04320d66a1f4d1a57c8b7cbd7304
zslazar <zs.lazar@gmail.com>
2017-12-12 10:02:43 +0100

    exercise 06

---
+++ week-04/day-01/06-Animals/06-Animals/Animal.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _06_Animals
+{
+    class Animal
+    {
+        public int Hunger { get; set; }
+        public int Thirst { get; set; }
+
+        public Animal()
+        {
+            Hunger = 50;
+            Thirst = 50;
+        }
+
+        public void Eat()
+        {
+            Hunger -= 1;
+        }
+
+        public void Drink()
+        {
+
+            Thirst -= 1;
+        }
+
+        public void Play ()
+        {
+            Hunger += 1;
+            Thirst += 1;
+        }
+    }
+}
---
+++ week-04/day-01/06-Animals/06-Animals/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _06_Animals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var animal01= new Animal();
+
+            Console.WriteLine("Hunger rate: " + animal01.Hunger);
+            animal01.Eat();
+            Console.WriteLine("New hunger rate: " + animal01.Hunger);
+            Console.ReadLine();
+
+            
+        }
+    }
+}
