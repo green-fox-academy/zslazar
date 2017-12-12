8188be5e92ea3f0491719edda16cb4324558a048
zslazar <zs.lazar@gmail.com>
2017-12-12 17:34:06 +0100

    join lists

---
+++ week-02/day-03/List02Matchmaking/List02Matchmaking/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace List02Matchmaking
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+            var girls = new List<string> { "Eve", "Ashley", "Bözsi", "Kat", "Jane" };
+            var boys = new List<string> { "Joe", "Fred", "Béla", "Todd", "Neef", "Jeff" };
+            var order = new List<string>();
+
+            // Join the two lists by matching one girl with one boy in the order list
+            // Exepected output: "Eve", "Joe", "Ashley", "Fred"...
+
+            for (int i = 0; i < girls.Count; i++)
+            {
+                order.Add(girls[i]);
+                order.Add(boys[i]);
+            }
+
+            foreach (var member in order)
+            {
+            Console.WriteLine(member);
+            }
+            Console.ReadLine();
+        }
+    }
+}
