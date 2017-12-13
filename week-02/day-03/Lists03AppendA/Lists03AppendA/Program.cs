d623605369dbca5d74592f440209b501a917e2dd
zslazar <zs.lazar@gmail.com>
2017-12-13 23:19:56 +0100

    list, for loop, foreach

---
+++ week-02/day-03/Lists03AppendA/Lists03AppendA/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lists03AppendA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Add "a" to every string in the far list.
+            var far = new List<string> { "kuty", "macsk", "kacs", "rók", "halacsk" };
+            var newlist = new List<string>();
+
+            for (int i = 0; i < far.Count; i++)
+            {
+                newlist.Add(far[i] + "a");
+            }
+
+            foreach (var member in newlist)
+            {
+                Console.WriteLine(member);
+            }
+            Console.ReadLine();
+        }
+    }
+}
