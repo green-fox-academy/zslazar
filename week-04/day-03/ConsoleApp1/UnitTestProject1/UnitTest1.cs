f42a04f1d082dea24049c0f4819fc579000aca4b
zslazar <zs.lazar@gmail.com>
2017-12-13 13:47:58 +0100

    exercise 1 values usage

--- week-04/day-03/ConsoleApp1/UnitTestProject1/UnitTest1.cs
+++ week-04/day-03/ConsoleApp1/UnitTestProject1/UnitTest1.cs
@@ -19,6 +19,18 @@ namespace UnitTestProject1
 
             //assert
             Assert.AreEqual(message, output);
+
+            //////////////////////////////////
+            //megadod az ertekeket es h azokat h minek akarod megfeleltetni
+            //lenti pelda 1,2,3 mint x es 4,5,6 etrek mint y
+            MathTask m = new MathTask();
+            public void Add([Values(1, 2, 3)] double x [Values(4, 5, 6)] double y)
+            {
+                var result = m.Add(x, y);
+                Assert.AreEqual(10, x);
+            }
+
         }
+
     }
 }
