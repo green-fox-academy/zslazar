43fd0655f6893f12c9841be81c21862012aea8c6
zslazar <zs.lazar@gmail.com>
2017-12-13 09:20:26 +0100

    sharpie class method

---
+++ week-04/day-01/07-Sharpie/07-Sharpie/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_Sharpie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var sharpie = new Sharpie("Black", 6);
+            sharpie.Use();
+            sharpie.Use();
+
+            Console.WriteLine(sharpie.InkAmount);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ week-04/day-01/07-Sharpie/07-Sharpie/Sharpie.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_Sharpie
+{
+    // We should know about each sharpie their Color(which should be a string), Width(which will be a floating point number), InkAmount(another floating point number)
+    // When creating one, we need to specify the Color and the Width
+    // Every sharpie is created with a default 100 as InkAmount
+    // We can Use() the sharpie objects
+    // which decreases inkAmount
+    class Sharpie
+    {
+        public string Color { get; set; }
+        public float Width { get; set; }
+        public float InkAmount { get; set; }
+
+        public Sharpie(string color, float width)
+        {
+            Color = color;
+            Width = width;
+            InkAmount = 100;
+        }
+
+        public void Use()
+        {
+            InkAmount -= 1;
+        }
+    }
+}
