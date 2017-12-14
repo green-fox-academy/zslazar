a80d756f4c19113783339cb45f849da2e43ba534
zslazar <zs.lazar@gmail.com>
2017-12-14 09:37:31 +0100

    list find an element

---
+++ week-02/day-03/Lists05ElementFinder/Lists05ElementFinder/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lists05ElementFinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+            // Check if the arrayList contains "7" if it contains print "Hoorray" otherwise print "Noooooo"
+            // Do this again with a different solution using different list functions!
+        {
+            var List = new List<int> { 1, 2, 7, 4, 5 };
+
+            if (List.Contains(7))
+            {
+                Console.WriteLine("Hoorray");
+            }
+            else
+            {
+                Console.WriteLine("Noooooo");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
