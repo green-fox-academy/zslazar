62bb214539934c75f17ccfa5b0290c062b67f154
zslazar <zs.lazar@gmail.com>
2017-12-14 09:13:06 +0100

    list add remove elements

---
+++ week-02/day-03/Lists04Candyshop/Lists04Candyshop/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lists04Candyshop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Accidentally we added "2" and "false" to the list. 
+            // Your task is to change from "2" to "Croissant" and change from "false" to "Ice cream"
+            // No, don't just remove the lines
+
+            var list = new List<object>();
+            list.Add("Cupcake");
+            list.Add(2);
+            list.Add("Brownie");
+            list.Add(false);
+
+            list.RemoveAt(1);
+            list.Insert(1, "Croissant");
+            list.Remove(false);
+            list.Add("Ice cream");
+
+            foreach (string elements in list)
+            {
+                Console.WriteLine(elements);
+            }
+                Console.ReadLine();
+        }
+    }
+}
