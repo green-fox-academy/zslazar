a65c307bfb21a068cfdd24c8c20d9f7dcb712432
zslazar <zs.lazar@gmail.com>
2017-12-18 22:04:04 +0100

    feat(Counter): create methods

---
+++ week-04/day-01/Counter/Counter/Counter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Counter
+{
+    class Counter
+    {
+        public int Number { get; set; }
+
+        public Counter()
+        {
+            Number = 0;
+        }
+
+        public Counter(int number)
+        {
+            Number = number;
+        }
+
+        public void Add()
+        {
+            Number++;
+        }
+
+        public void Add(int number)
+        {
+            Number += number;
+        }
+
+        public string Get()
+        {
+            return Number.ToString();
+        }
+
+        public int Reset()
+        {
+            return Number = 0;
+        }
+    }
+}
---
+++ week-04/day-01/Counter/Counter/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+//Create Counter class
+//which has an integer property
+//when creating it should have a default value 0 or we can specify it when creating
+//we can Add(number) to this counter another whole number
+//or we can Add() without parameters just increasing the counter's value by one
+//and we can Get() the current value as string
+//also we can Reset() the value to the initial value
+
+namespace Counter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var value = new Counter();
+
+            value.Add(5);
+
+
+            Console.WriteLine(value.Get());
+
+            value.Reset();
+            Console.WriteLine(value.Get());
+
+            Console.ReadLine();
+        }
+    }
+}
