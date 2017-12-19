5fea05489d1eb8a37221f8c8aeb773d8d642c0e3
zslazar <zs.lazar@gmail.com>
2017-12-19 14:33:45 +0100

    feat(BookPages): new features

--- week-04/day-01/BookPages/BookPages/Program.cs
+++ week-04/day-01/BookPages/BookPages/Program.cs
@@ -12,7 +12,13 @@ namespace BookPages
             b.Add(new Page { Content = string.Empty });
             b.Add(new Page { Content = "This is the second page" });
 
+            Book c = new Book();
+            c.Add(new Page { Content = "oh yes"});
+            c.Add(new Page { Content = "" });
+            c.Add(new Page { Content = string.Empty });
+
             Console.WriteLine(b.CountBlankPages());
+            Console.WriteLine(c.CountBlankPages());
             Console.ReadLine();
         }
     }
