6fd7621f2b33676864f65f4dcab54576c923a880
zslazar <zs.lazar@gmail.com>
2017-12-19 11:35:56 +0100

    feat(PetrolStation): create methods

---
+++ week-04/day-01/PetrolStation/PetrolStation/Car.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PetrolStation
+{
+    // Car
+    // GasAmount
+    // Capacity
+    // create constructor for Car where:
+    // initialize gasAmount -> 0
+    // initialize capacity -> 100
+    
+    class Car
+    {
+    public int GasAmount { get; set; }
+    public int Capacity { get; set; }
+
+    public Car()
+        {
+            GasAmount = 0;
+            Capacity = 100;
+        }
+
+
+
+
+    }
+}
---
+++ week-04/day-01/PetrolStation/PetrolStation/Station.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PetrolStation
+{
+    // Station
+    // GasAmount
+    // Refill(car) -> decreases the gasAmount by the capacity of the car 
+    // and increases the cars gasAmount
+
+    class Station
+    {
+        public int GasAmount { get; set; }
+
+        public void Refill(Car c)
+        {
+            c.GasAmount++;
+            c.Capacity--;
+        }
+    }
+}
