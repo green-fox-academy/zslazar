3522eb976686fb96de74f184d7827de9dc135cba
zslazar <zs.lazar@gmail.com>
2017-12-19 11:57:17 +0100

    feat(SharpieSet): create solution

---
+++ week-04/day-01/SharpieSet/SharpieSet/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SharpieSet
+{
+    class Program
+    {
+    // Sharpie Set
+    // Reuse your Sharpie class
+    // Create SharpieSet class
+    // it contains a list of Sharpie
+    // Add method CountUsable() -> sharpie is usable if it has ink in it
+    // Add method RemoveTrash() -> removes all unusable sharpies
+
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
+
