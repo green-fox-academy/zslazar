d9ff339e153e9d69bfdc1bcb383d7634fb298566
zslazar <zs.lazar@gmail.com>
2017-12-21 15:39:50 +0100

    feat(Garden) create classes :seedling:

---
+++ week-04/day-02/GardenApp/GardenApp/Flower.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GardenApp
+{
+    //    needs water if its current water amount is less then 5
+    //when watering it the flower can only absorb the 75% of the water
+    //eg.watering with 10 the flower's amount of water should only increase with 7.5
+    class Flower : Garden
+    {
+    }
+}
---
+++ week-04/day-02/GardenApp/GardenApp/Garden.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GardenApp
+{
+    //    is able to hold unlimited amount of flowers or trees
+    //when watering it should only water those what needs water with equally divided amount amongst them
+    //eg.watering with 40 and 4 of them need water then each gets watered with 10
+    class Garden
+    {
+    }
+}
