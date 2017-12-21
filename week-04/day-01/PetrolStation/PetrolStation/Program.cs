9e63fa2b31ed8c625923f7f99aad247fe61afa6c
zslazar <zs.lazar@gmail.com>
2017-12-21 10:20:00 +0100

    refactor(PetrolStation) :car:

--- week-04/day-01/PetrolStation/PetrolStation/Program.cs
+++ week-04/day-01/PetrolStation/PetrolStation/Program.cs
@@ -4,17 +4,6 @@ namespace PetrolStation
 {
     class Program
     {
-    // Create Station and Car classes
-    // Station
-    // GasAmount
-    // Refill(car) -> decreases the gasAmount by the capacity of the car and increases the cars gasAmount
-    // Car
-    // GasAmount
-    // Capacity
-    // create constructor for Car where:
-    // initialize gasAmount -> 0
-    // initialize capacity -> 100
-
         static void Main(string[] args)
         {
             Console.WriteLine();
