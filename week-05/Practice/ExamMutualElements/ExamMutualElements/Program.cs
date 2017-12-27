326e2da9cee8eb9de11c5983c027a7693463f1fc
zslazar <zs.lazar@gmail.com>
2017-12-27 18:24:42 +0100

    practice(exam)tests added

--- week-05/Practice/ExamMutualElements/ExamMutualElements/Program.cs
+++ week-05/Practice/ExamMutualElements/ExamMutualElements/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace MutualElements
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
--- week-05/Practice/ExamMutualElements/UnitTests/UnitTest1.cs
+++ week-05/Practice/ExamMutualElements/UnitTests/UnitTest1.cs
@@ -1,6 +1,6 @@
 using MutualElements;
 using NUnit.Framework;
-
+using System.Collections.Generic;
 
 namespace UnitTests
 {
@@ -8,8 +8,55 @@ namespace UnitTests
     public class UnitTest1
     {
         [Test]
-        public void TestMethod1()
+        public void MutualElements()
         {
+            //Ha nem lenne static a method, akkor példányosítani kell az osztályt
+            //Program program = new Program();
+
+            //arrange
+            List<int> first = new List<int>() { 2, 4, 6, 8, 1 };
+            List<int> second = new List<int>() { 3, 5, 7, 9, 1 };
+            List<int> expectedList = new List<int>() { 1 };
+
+            //Ha nem lenne static a method, akkor példányosítani kell az osztályt
+            //var result = program.FilterMutualElements(first, second);
+
+            //egyébként így lehet elérni a methodot:
+            //act
+            var result = Program.FilterMutualElements(first, second);
+
+            //assert
+            Assert.AreEqual(expectedList, result);
+        }
+
+        [Test]
+        public void NonMutualElements()
+        {
+            //arrange
+            List<int> first = new List<int>() { 2, 4, 6, 8 };
+            List<int> second = new List<int>() { 3, 5, 7, 9 };
+            List<int> expectedList = new List<int>() { };
+
+            //act
+            var result = Program.FilterMutualElements(first, second);
+
+            //assert
+            Assert.AreEqual(expectedList, result);
+        }
+
+        [Test]
+        public void SameListMutualElements()
+        {
+            //arrange
+            List<int> first = new List<int>() { 1, 2, 1, 2, 3, 1, 2, 3, 1, 3, 2, 1, 2, 2, 3, 3 };
+            List<int> second = new List<int>() { 1, 2, 1, 2, 3, 1, 2, 3, 1, 3, 2, 1, 2, 2, 3, 3 };
+            List<int> expectedList = new List<int>() { 1, 2, 3 };
+
+            //act
+            var result = Program.FilterMutualElements(first, second);
+
+            //assert
+            Assert.AreEqual(expectedList, result);
         }
     }
 }
