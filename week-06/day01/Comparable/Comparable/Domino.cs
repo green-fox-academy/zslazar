e0d7c0899b178440d472837268ec76e23d2a3220
zslazar <zs.lazar@gmail.com>
2018-01-02 17:35:09 +0100

    feat(comparable) IComparable, Compare To method

---
+++ week-06/day01/Comparable/Comparable/Domino.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Comparable
+{
+    public class Domino : IComparable
+    {
+        private readonly int[] Values;
+
+        public Domino(int valueA, int valueB)
+        {
+            this.Values = new int[] { valueA, valueB };
+        }
+
+        public int[] GetValues()
+        {
+            return Values;
+        }
+
+        public int CompareTo(Domino domino)
+        {
+            int number = 0;
+            if (domino.Values[0] < Values[0])
+            {
+
+            }
+
+
+        }
+    }
+}
