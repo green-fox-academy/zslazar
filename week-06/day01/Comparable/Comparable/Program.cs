e72c31f6720fb66a5a2644fe9b10608be9fc250f
zslazar <zs.lazar@gmail.com>
2018-01-02 21:14:12 +0100

    feat(comparable) IComparable, CompareTo method

--- week-06/day01/Comparable/Comparable/Program.cs
+++ week-06/day01/Comparable/Comparable/Program.cs
@@ -7,6 +7,8 @@ namespace Comparable
     {
         static void Main(string[] args)
         {
+            //var dominoes = InitializeDominoes();
+
             List<Domino> dominoes = new List<Domino>();
             dominoes.Add(new Domino(5, 2));
             dominoes.Add(new Domino(4, 6));
@@ -24,5 +26,22 @@ namespace Comparable
 
             Console.ReadKey();
         }
+
+        /*
+         public static List<Domino> InitializeDominoes()
+        {
+            var dominoes = new List<Domino>
+            {
+                new Domino(5, 2),
+                new Domino(4, 6),
+                new Domino(1, 5),
+                new Domino(6, 7),
+                new Domino(2, 4),
+                new Domino(7, 1),
+                new Domino(6, 4)
+            };
+            return dominoes;
+        }
+      */
     }
 }
