7202d4cdd06141a2e876296d65b44a5dc1fc7381
zslazar <zs.lazar@gmail.com>
2018-01-03 17:42:15 +0100

    feat(linq)even numbers squared value

---
+++ week-06/day02/ExerciseThree/ExerciseThree/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace ExerciseThree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a LINQ Expression to get the squared value of the positive numbers from the following array:
+
+            int[] numbers = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
+                        
+            var positiveEvenNumbers = numbers.Where(x => x % 2 == 0)
+                .Select(num => Math.Pow(num, 2)).ToList();
+
+            positiveEvenNumbers.ForEach(i => Console.WriteLine($"[{i}]"));
+
+
+            Console.ReadLine();
+        }
+    }
+}
