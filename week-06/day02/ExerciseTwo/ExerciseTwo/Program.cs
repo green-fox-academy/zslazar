7d160735853ae7057ba9266a6bb1649124ed1dc9
zslazar <zs.lazar@gmail.com>
2018-01-03 16:12:57 +0100

    feat(linq)odd numbers average with query & method syntax :bamboo:

---
+++ week-06/day02/ExerciseTwo/ExerciseTwo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace ExerciseTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a LINQ Expression to get the average value of the odd numbers from the following array:
+
+            int[] numbers = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
+
+            //query syntax
+            var number = (from n in numbers
+                          where n % 2 != 0
+                          select n).Average();
+
+            //output
+                Console.WriteLine($"[{number}]");
+
+            //method syntax
+            //var numberMethOddAverage = numbers.Where(x => (x % 2 != 0)).Average();
+            var numberMethOddAverage = numbers.Where(x => (x % 2 == 1 || x % 2 == -1)).Average();
+
+            //output
+            Console.WriteLine("method syntax output: " + numberMethOddAverage);
+            
+            Console.ReadLine();
+        }
+    }
+}
