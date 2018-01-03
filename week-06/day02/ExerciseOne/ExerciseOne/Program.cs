ad07e85467afc434d9049dc5c92362e27a6344dd
zslazar <zs.lazar@gmail.com>
2018-01-03 15:14:45 +0100

    feat(linq)even numbers with query & method syntax

---
+++ week-06/day02/ExerciseOne/ExerciseOne/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace ExerciseOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
+
+            //query syntax
+            var number = from n in numbers
+                         where n % 2 == 0
+                         orderby n
+                         select n;
+            //output 
+            Console.WriteLine("query syntax output");
+            foreach (var n in number)
+            {
+                Console.WriteLine($"[{n}]");
+            }
+            
+            //method syntax
+            var numbermethod = number.Where(x => (x % 2 == 0)).OrderBy(x => x);
+
+            //output
+            Console.WriteLine("method syntax output");
+            foreach (var n1 in numbermethod)
+            {
+                Console.WriteLine($"[{n1}]");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
