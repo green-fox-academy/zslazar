ee0fb61fbf1da2c0151588862768cfcc744086c7
zslazar <zs.lazar@gmail.com>
2018-01-11 14:28:35 +0100

    feat(CounterProject) web click counter

---
+++ week-07/day04/CounterProject/CounterProject/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CounterProject.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CounterProject.Controllers
+{
+    [Route("")]
+    public class HomeController : Controller
+    {
+        private Counter counter;
+
+        public HomeController(Counter counter)
+        {
+            this.counter = counter;
+        }
+        
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            return View(counter);
+        }
+
+        [HttpPost("add")]
+        public IActionResult Counter()
+        {
+            counter.ClickCounter++;
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ week-07/day04/CounterProject/CounterProject/Models/Counter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CounterProject.Models
+{
+    public class Counter
+    {
+        public long ClickCounter { get; set; } = 2;
+
+        //public CCounter(long clickcounter)
+        //{
+        //    ClickCounter = clickcounter;
+        //}
+    }
+}
