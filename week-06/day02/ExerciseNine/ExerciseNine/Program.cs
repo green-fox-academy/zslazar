b43d9f59a857cf753dd37eeae57f0e28738f8249
zslazar <zs.lazar@gmail.com>
2018-01-14 00:46:08 +0100

    feat(LINQ9)char to string

---
+++ week-06/day02/ExerciseNine/ExerciseNine/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace ExerciseNine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a LINQ Expression to convert a char array to a string!
+
+            char[] elements = { 'n', 'e', 'w', 'y', 'o', 'r', 'k' };
+
+            //solution1
+            string word = string.Join(",", elements).ToString();
+            //string word = String.Join(",", elements).ToString();
+            Console.WriteLine(word);
+
+            //solution2
+            string word2 = string.Join(string.Empty, elements);
+            Console.WriteLine(word2);
+
+            Console.ReadLine();
+        }
+    }
+}
