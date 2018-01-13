a2dfabeb930033649a791ed008a3705d8b67c826
zslazar <zs.lazar@gmail.com>
2018-01-13 23:55:48 +0100

    feat(LINQ5)frequency of numbers

---
+++ week-06/day02/ExerciseFive/ExerciseFive/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace ExerciseFive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a LINQ Expression to find the frequency of numbers in the following array:
+            int[] numbers = new int[] { 5, 9, 1, 2, 3, 7, 5, 6, 7, 3, 7, 6, 8, 5, 4, 9, 6, 2 };
+
+            var frequencyOfNumbers = numbers.OrderByDescending(n => n).GroupBy(n => n)
+               .ToDictionary(n => n.Key, n => n.Count());
+
+            foreach (var item in frequencyOfNumbers)
+
+            Console.WriteLine($"{item.Key} {item.Value}");
+            Console.ReadLine();
+
+        }
+    }
+}
