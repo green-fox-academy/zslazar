c068e7ec17703dd48a0c5491e58db179fa6f7c8f
zslazar <zs.lazar@gmail.com>
2018-01-13 23:46:33 +0100

    feat(LINQ4)square number new solution

--- week-06/day02/ExerciseFour/ExerciseFour/Program.cs
+++ week-06/day02/ExerciseFour/ExerciseFour/Program.cs
@@ -11,6 +11,12 @@ namespace ExerciseFour
 
             int[] numbers = new[] { 3, 9, 2, 8, 6, 5 };
 
+            //solution1
+            var SquaredNumber = numbers.Where(num => Math.Pow(num, 2) > 20).ToList();
+            //var highSquaredNumber = n.Where(num => Math.Pow(num, 2) > 20).Select(x => Math.Pow(x, 2)).ToList();
+            SquaredNumber.ForEach(i => Console.WriteLine($"[{i}]"));
+
+            //solution2
             numbers.Where(n => n * n > 20).ToList().ForEach(n => Console.WriteLine($"[{n}]"));
             Console.ReadLine();
         }
