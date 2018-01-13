53b843e00d68fc0220c807c063b00f0db5ba2186
zslazar <zs.lazar@gmail.com>
2018-01-13 23:23:57 +0100

    feat(LINQ3)new solution

--- week-06/day02/ExerciseThree/ExerciseThree/Program.cs
+++ week-06/day02/ExerciseThree/ExerciseThree/Program.cs
@@ -11,12 +11,14 @@ namespace ExerciseThree
 
             int[] numbers = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
                         
-            var positiveEvenNumbers = numbers.Where(x => x % 2 == 0)
+            var positiveEvenNumbers = numbers.Where(x => x > 0)
                 .Select(num => Math.Pow(num, 2)).ToList();
 
             positiveEvenNumbers.ForEach(i => Console.WriteLine($"[{i}]"));
+            
 
-
+            //solution2
+            numbers.Where(n => n > 0).Select(n => n * n).ToList().ForEach(n => Console.WriteLine(n));
             Console.ReadLine();
         }
     }
