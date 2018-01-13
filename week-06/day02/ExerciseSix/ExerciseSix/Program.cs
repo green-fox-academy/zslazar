c4618f363e96195cb3b4b527115baf9804af8dd3
zslazar <zs.lazar@gmail.com>
2018-01-14 00:09:07 +0100

    feat(LINQ6)frequency of characters

---
+++ week-06/day02/ExerciseSix/ExerciseSix/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace ExerciseSix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a LINQ Expression to find the frequency of characters in a given string!
+
+            string sting = "Englishman in New York";
+
+            var frequencyOfLetters = sting.GroupBy(n => n)
+               .ToDictionary(n => n.Key, n => n.Count());
+
+            foreach (var item in frequencyOfLetters)
+            
+            Console.WriteLine($"{item.Key} {item.Value}");
+            Console.ReadLine();
+        }
+    }
+}
