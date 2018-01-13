e810ff5f17c34946d801492ac45b63f4a95d0472
zslazar <zs.lazar@gmail.com>
2018-01-13 23:42:13 +0100

    feat(LINQ4)square number

---
+++ week-06/day02/ExerciseFour/ExerciseFour/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace ExerciseFour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a LINQ Expression to find which number squared value is more then 20 from the following array:
+
+            int[] numbers = new[] { 3, 9, 2, 8, 6, 5 };
+
+            numbers.Where(n => n * n > 20).ToList().ForEach(n => Console.WriteLine($"[{n}]"));
+            Console.ReadLine();
+        }
+    }
+}
