61cc599b9b63ce618054ef09ba6187ae70e9cd38
zslazar <zs.lazar@gmail.com>
2018-01-15 15:01:27 +0100

    feat(ListingTodos)create task

---
+++ week-08/day01/ListingTodos/ListingTodos/Controllers/TodoController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ListingTodos.Repositories;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ListingTodos.Controllers
+{
+    [Route("")]
+    public class TodoController : Controller
+    {
+
+        private TodoRepository repo;
+
+        public TodoController(TodoRepository repo)
+        {
+            this.repo = repo;
+        }
+               
+        [HttpGet("todo")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("")]
+        [HttpGet("list")]
+        public IActionResult List()
+        {
+            //var text = "This is my first todo";
+            //return Json(text);
+
+            return View(repo.todoList);
+
+        }
+    }
+}
---
+++ week-08/day01/ListingTodos/ListingTodos/Repositories/TodoRepository.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ListingTodos.Repositories
+{
+    public class TodoRepository
+    {
+        //public TodoContext todoContext;
+
+        //public TodoRepository(TodoContext todoContext)
+        //{
+        //    this.todoContext = todoContext;
+        //}
+
+        public List<Todo> todoList = new List<Todo>()
+        {
+            new Todo() { Title = "Start the day" },
+            new Todo() { Title = "Finish H2 workshop1" },
+            new Todo() { Title = "Finish JPA workshop2" },
+            new Todo() { Title = "Create a CRUD project" }
+        };
+
+        //public TodoRepository()
+        //{
+        //    InitTodos();
+        //}
+
+        //private void InitTodos()
+        //{
+        //    todoList.Add(new Todo()
+        //    {
+        //        Title = "Start the day"
+        //    });
+
+        //    todoList.Add(new Todo()
+        //    {
+        //        Title = "Finish H2 workshop1"
+        //    });
+
+        //    todoList.Add(new Todo()
+        //    {
+        //        Title = "Finish JPA workshop2"
+        //    });
+
+        //    todoList.Add(new Todo()
+        //    {
+        //        Title = "Create a CRUD project"
+        //    });
+        //}
+
+    }
+}
