ce771e03af082583411d5c7c0260c6412dea886a
zslazar <zs.lazar@gmail.com>
2018-01-21 01:02:21 +0100

    feat(DogSheetWebApp) design & refactor :sparkles: :dog2:

--- week-08/day05/DogSheetWA/DogSheetWA/Controllers/HomeController.cs
+++ week-08/day05/DogSheetWA/DogSheetWA/Controllers/HomeController.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using DogSheetWA.Models;
 using DogSheetWA.Repositories;
-using DogSheetWA.Models;
+using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
--- week-08/day05/DogSheetWA/DogSheetWA/Startup.cs
+++ week-08/day05/DogSheetWA/DogSheetWA/Startup.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using DogSheetWA.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.DependencyInjection;
-using DogSheetWA.Repositories;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
 
 namespace DogSheetWA
