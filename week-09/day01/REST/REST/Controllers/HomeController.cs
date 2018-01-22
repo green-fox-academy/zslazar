ad8479e5dfd75c1a2116e342ed5042d579dcedf5
zslazar <zs.lazar@gmail.com>
2018-01-22 11:48:26 +0100

    refaktor(WebAPI) frontend

--- week-09/day01/REST/REST/Controllers/HomeController.cs
+++ week-09/day01/REST/REST/Controllers/HomeController.cs
@@ -5,8 +5,6 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-//Create a HomeController with a single endpoint to /
-//The method should return File("index.html", "text/html") here
 
 
 namespace REST.Controllers
@@ -14,12 +12,27 @@ namespace REST.Controllers
     [Route("api/[controller]")]
     public class HomeController : Controller
     {
-        [Route("")]
-        [HttpGet]
+        //Create a HomeController with a single endpoint to /
+        //The method should return File("index.html", "text/html") here
+
+        
+        [HttpGet("")]
         public IActionResult GetFile()
         {
             return File("index.html", "text/html");
         }
+
+        /* Create a GET /doubling endpoint
+        That receives a query parameter: input=15
+        And responds with a json object with the doubled amount of the input:
+        {
+          "received": 15,
+          "result": 30
+        }
+        if no input is provided:
+        {
+          "error": "Please provide an input!"
+        }*/
                 
     }
 }
