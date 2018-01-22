740c413a30dd883e4990224694b069de679ac577
zslazar <zs.lazar@gmail.com>
2018-01-22 11:43:08 +0100

    feat(WebAPI) frontend

---
+++ week-09/day01/REST/REST/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+//Create a HomeController with a single endpoint to /
+//The method should return File("index.html", "text/html") here
+
+
+namespace REST.Controllers
+{
+    [Route("api/[controller]")]
+    public class HomeController : Controller
+    {
+        [Route("")]
+        [HttpGet]
+        public IActionResult GetFile()
+        {
+            return File("index.html", "text/html");
+        }
+                
+    }
+}
