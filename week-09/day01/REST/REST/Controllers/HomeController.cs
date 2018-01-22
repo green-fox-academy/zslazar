625fc918d4b3d3f0b9c863d47edb0a6549d89e0b
zslazar <zs.lazar@gmail.com>
2018-01-22 14:23:17 +0100

    feat(WebAPI) greeter :bowtie:

--- week-09/day01/REST/REST/Controllers/HomeController.cs
+++ week-09/day01/REST/REST/Controllers/HomeController.cs
@@ -6,7 +6,6 @@ using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-
 namespace REST.Controllers
 {
     [Route("")]
@@ -14,16 +13,14 @@ namespace REST.Controllers
     {
         //Create a HomeController with a single endpoint to /
         //The method should return File("index.html", "text/html") here
-
-        
+                
         [HttpGet("/")]
         public IActionResult GetFile()
         {
             return File("index.html", "text/html");
         }
 
-        /* Create a GET /doubling endpoint
-        That receives a query parameter: input=15
+        /* Create a GET /doubling endpoint That receives a query parameter: input=15
         And responds with a json object with the doubled amount of the input:
         {
           "received": 15,
@@ -43,9 +40,31 @@ namespace REST.Controllers
             {
                 return Json(new { error = "Please provide an input!" });
             }
-            return Json(new { received = input, result = input * 2 });
-                       
+            return Json(new { received = input, result = input * 2 });                       
         }
 
+        /* Create a GET /greeter endpoint that receives a query parameter name = Petike & title = student
+        and responds with an awesome greeting json object:
+        {
+          "welcome_message": "Oh, hi there Petike, my dear student!"
+        }
+        if e.g.no name(or title) is provided:
+        {
+          "error": "Please provide a name!"
+        }  */
+
+        [HttpGet("greeter")]
+        public IActionResult Greeter([FromQuery] string name, [FromQuery] string title)
+        {
+            if (name == null)
+            {
+                return Json(new { error = "Please provide a name!" });
+            }
+            if (title == null)
+            {
+                return Json(new { error = "Please provide a title!" });
+            }
+            return Json(new { welcome_message = $"Oh, hi there {name}, my dear {title}!" });
+            }
     }
 }
