69afd24fcbaca96a1b7b3602a9100decdc5d610a
zslazar <zs.lazar@gmail.com>
2018-01-24 14:12:32 +0100

    feat(Groot) groot endpoint

---
+++ week-09/day03/Groot/Groot/Controllers/GrootController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Groot.Controllers
+{
+    [Route("")]
+    public class GrootController : Controller
+    {
+        // GET: /<controller>/
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        [HttpGet("groot")]
+        public IActionResult Groot([FromQuery] string message)
+        {
+            if (message == null)
+            {
+                return Json(new { error = "I am Groot!" });
+            }
+            return Json(new { received = message, translated = "I am Groot!" });
+
+        }
+    }
+}
