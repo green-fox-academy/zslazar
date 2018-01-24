164356cb599fbb652e7c48ee21c45c677811fecd
zslazar <zs.lazar@gmail.com>
2018-01-24 17:31:49 +0100

    feat(Yondu) tests added :shipit:

--- week-09/day03/Yondu/YonduTestProject/YonduTest.cs
+++ week-09/day03/Yondu/YonduTestProject/YonduTest.cs
@@ -1,14 +1,44 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
 using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
 using Xunit;
+using Yondu;
 
 namespace YonduTestProject
 {
     public class YonduTest
     {
-        [Fact]
-        public void Test1()
-        {
+        private readonly TestServer server;
+        private readonly HttpClient client;
 
+        public YonduTest()
+        {
+            server = new TestServer(new WebHostBuilder().UseStartup<Startup>());
+            client = server.CreateClient();
         }
+
+        [Fact]
+        public async Task ReturnOkStatus()
+        {
+            var response = await client.GetAsync("yondu?distance=100&time=10");
+            string json = await response.Content.ReadAsStringAsync();
+            Assert.Equal("{\"distance\":100.0,\"time\":10.0,\"speed\":10.0}", json);
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task ReturnBadRequestStatus()
+        {
+            var response = await client.GetAsync("yondu?distance=100");
+            string json = await response.Content.ReadAsStringAsync();
+            Assert.Equal("{\"error\":\"Error occured!\"}", json);
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        }
+
     }
 }
