685e1c6cd7885c5e94cebe0772d81f412463b164
zslazar <zs.lazar@gmail.com>
2018-01-24 17:14:33 +0100

    feat(Yondu) create task :small_blue_diamond:

---
+++ week-09/day03/Yondu/Yondu/Controllers/YonduController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Yondu.Controllers
+{
+    [Route("")]
+    public class YonduController : Controller
+    {
+
+        //http://localhost:54916/yondu?distance=10&time=10
+        [HttpGet("yondu")]
+        public IActionResult Yondu([FromQuery]double? distance, [FromQuery] double? time)
+        {
+            if (distance is null || time is null || time == 0)
+            {
+                return BadRequest(new { error = "Error occured!" });
+            }
+            return Json(new { distance = distance, time = time, speed = distance / time });
+        }
+    }
+}
---
+++ week-09/day03/Yondu/YonduTestProject/YonduTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace YonduTestProject
+{
+    public class YonduTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
