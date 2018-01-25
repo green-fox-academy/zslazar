cd14485324b127975594ea507d0bf502dc56a120
zslazar <zs.lazar@gmail.com>
2018-01-25 12:05:53 +0100

    feat(RedditBE) base structure

--- week-09/day04/RedditBE/RedditBE/Models/Posts.cs
+++ week-09/day04/RedditBE/RedditBE/Models/Posts.cs
@@ -7,5 +7,10 @@ namespace RedditBE.Models
 {
     public class Posts
     {
+        public int Id { get; set; }
+        public int Title { get; set; }
+        public int Url { get; set; }
+        public int Timestamp { get; set; }
+        public int Score { get; set; }
     }
 }
