1893e9f8f184b85039f03ce75959a4054a4fce8c
zslazar <zs.lazar@gmail.com>
2018-01-31 23:29:20 +0100

    feat(WareHouse) Json arrived :feelsgood:

--- week-10/day03/WareHouse/WareHouse/ViewModels/ClothesViewModel.cs
+++ week-10/day03/WareHouse/WareHouse/ViewModels/ClothesViewModel.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using WareHouse.Models;
 
 namespace WareHouse.ViewModels
