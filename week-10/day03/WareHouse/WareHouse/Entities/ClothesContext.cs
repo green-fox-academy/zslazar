cce52d960cdb9350a4b322534bf6e339c86f715a
zslazar <zs.lazar@gmail.com>
2018-01-31 14:12:15 +0100

    feat(WareHouse) base structure :boot:

---
+++ week-10/day03/WareHouse/WareHouse/Entities/ClothesContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WareHouse.Models;
+
+namespace WareHouse.Entities
+{
+    public class ClothesContext : DbContext
+    {
+        public ClothesContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Clothes> WarehouseDB { get; set; }
+    }
+}
---
+++ week-10/day03/WareHouse/WareHouse/Models/Clothes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WareHouse.Models
+{
+    public class Clothes
+    {
+        public long Id { get; set; }
+        public long ItemName { get; set; }
+        public long Manufacturer { get; set; }
+        public long Category { get; set; }
+        public long Size { get; set; }
+        public long UnitPrice { get; set; }
+    }
+}
