9d0f8c3c2ed035a507ade3f229d18dc78b2ebb4a
zslazar <zs.lazar@gmail.com>
2018-03-25 23:06:13 +0200

    feat(GuessMyNumber)

---
+++ week-02/day-05/GuessMyNumber/GuessMyNumber/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace GuessMyNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("You have 10 lives. Guess my number between 1 and 100");
+            int guessedNumber = int.Parse(Console.ReadLine());
+
+            Random rnd = new Random();
+            int number = rnd.Next(1, 100);
+            int lifeCounter = 10;
+
+            while ((lifeCounter > 0))
+            {
+                if (guessedNumber > number)
+                {
+                    lifeCounter--;
+                    if (lifeCounter > 0)
+                    {
+                        Console.WriteLine("Too high, You have " + lifeCounter + " life left!");                
+                    }
+                    else
+                    {
+                        Console.WriteLine("You managed to die!");
+                    }
+                }
+                else if (guessedNumber < number)
+                {
+                    lifeCounter--;
+                    if (lifeCounter > 0)
+                    {
+                        Console.WriteLine("Too low, You have " + lifeCounter + " life left!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("You managed to die!");
+                    }                                                         
+                }
+                else
+                {
+                    lifeCounter = 0;
+                    Console.WriteLine("Yes the number is: " + guessedNumber);
+                    Console.ReadLine();
+                }
+                guessedNumber = int.Parse(Console.ReadLine());
+            }
+                        
+            Console.ReadLine();
+        }
+    }
+}
