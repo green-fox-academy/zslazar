f94b0b795c17a5245bbac24521fb05a65d3bed7e
zslazar <zs.lazar@gmail.com>
2018-03-25 23:17:39 +0200

    feat(SubString)find the starting index of a substring

---
+++ week-02/day-04/SubString/SubString/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SubString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  Create a function that takes two strings as a parameter
+            //  Returns the starting index where the second one is starting in the first one
+            //  Returns `-1` if the second string is not in the first one
+
+            Console.WriteLine(FindTheSubstring("green fox academy", "fox"));
+            Console.ReadLine();
+        }
+
+        static int FindTheSubstring(string main, string sub)
+        {
+            for (int i = 0; i < main.Length - (sub.Length - 1); i++)
+            {
+                if (main.Substring(i, sub.Length) == sub)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+}
